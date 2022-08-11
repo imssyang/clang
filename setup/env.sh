409460cee292df15886dacfac61632383bab488c
imssyang <imssyang@gmail.com>
2022-08-11 23:13:01 +0800

    add env

--- setup/env.sh
+++ setup/env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export LLVM_DISABLE_ENV=yes
+export LLVM_INSTALL_DIR=/opt/llvm
 export LLVM_CONFIG=/opt/llvm/bin/llvm-config
 export LLVM_DIR=/opt/llvm/lib/cmake/llvm
 eval "optbin -s /opt/llvm/bin"
