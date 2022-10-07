9d02bf05abeee6b6845ed11f9ce41f09a54d3fea
imssyang <imssyang@gmail.com>
2022-10-07 23:12:37 +0800

    update sample/structure

--- sample/structure/linked_list/SLLGetKthFromEnd.cpp
+++ sample/structure/linked_list/SLLGetKthFromEnd.cpp
@@ -34,7 +34,7 @@ public:
     }
 };
 
-class DoublePointSolution {
+class IterationSolution {
 public:
     ListNode* getKthFromEnd(ListNode* head, int k) {
         ListNode *fast = head;
@@ -73,6 +73,22 @@ public:
         slow->next = slow->next->next;
         return head;
     }
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr) {
+            // 先存储后一个节点
+            ListNode* next = curr->next;
+            // 更新当前节点的下一个节点为前一个节点
+            curr->next = prev;
+            // 更新前一个节点为当前节点
+            prev = curr;
+            // 更新当前节点为下一个节点
+            curr = next;
+        }
+        return prev;
+    }
 };
 
 class RecursionSolution {
@@ -82,7 +98,7 @@ public:
         // 遇到倒数第一个节点时终止入栈，出栈时返回倒数第一个节点
         if (!head->next)
             return head;
-        // 从头节点开始入栈，出栈节点从倒数第一个节点开始
+        // 从第二个节点开始入栈，出栈节点从倒数第一个节点开始
         ListNode *node = getKthFromEnd(head->next, k);
         i++;
         if (i < k)
@@ -97,7 +113,7 @@ public:
         // 遇到NULL节点时终止入栈，出栈时返回NULL节点
         if (!head)
             return NULL;
-        // 从头节点开始入栈，出栈节点从倒数第零个节点（即NULL节点）开始，且在出栈后立即重建节点关系
+        // 从第二个节点开始入栈，出栈节点从倒数第零个节点（即NULL节点）开始，且在出栈后立即重建节点关系
         head->next = removeKthFromEnd(head->next, k);
         j++;
         if (j == k)
@@ -106,6 +122,19 @@ public:
         // 其余情况，返回入栈节点
         return head;
     }
+
+    ListNode* reverseList(ListNode* head) {
+        // 遇到倒数第一个节点时终止入栈，出栈时返回倒数第一个节点
+        if (!head || !head->next)
+            return head;
+        // 从第二个节点开始入栈，出栈节点从倒数第一个节点开始
+        ListNode* newHead = reverseList(head->next);
+        // 从倒数第一个节点到第二个节点，依次更新当前节点的下一个节点为前一个节点
+        head->next->next = head;
+        // 将前一个节点的下一个节点置为空（为了将旧头节点更新为新尾节点）
+        head->next = nullptr;
+        return newHead;
+    }
 };
 
 int main() {
@@ -113,11 +142,11 @@ int main() {
     ListNode *head = NULL;
     auto vec = vector<int>{6, 5, 4, 3, 2, 1};
 
-    cout << "getKthFromEnd-DoublePoint:" << endl;
+    cout << "getKthFromEnd-Iteration:" << endl;
     head = env.init(vec);
     for (int i = 1; i < vec.size(); i++) {
-        DoublePointSolution dps;
-        auto* kth = dps.getKthFromEnd(head, i);
+        IterationSolution is;
+        auto* kth = is.getKthFromEnd(head, i);
         env.print(kth);
     }
 
@@ -129,20 +158,36 @@ int main() {
         env.print(kth);
     }
 
-    cout << "removeKthFromEnd-DoublePoint:" << endl;
+    cout << "removeKthFromEnd-Iteration:" << endl;
     head = env.init(vec);
     for (int i = 1; i < vec.size(); i++) {
-        DoublePointSolution dps;
-        auto* kth = dps.removeKthFromEnd(head, i);
+        IterationSolution is;
+        auto* kth = is.removeKthFromEnd(head, i);
         env.print(kth);
     }
 
     cout << "removeKthFromEnd-Recursion:" << endl;
     head = env.init(vec);
     for (int i = 1; i < vec.size(); i++) {
-        DoublePointSolution dps;
-        auto* kth = dps.removeKthFromEnd(head, i);
+        RecursionSolution rs;
+        auto* kth = rs.removeKthFromEnd(head, i);
         env.print(kth);
+    }
+
+    cout << "reverseList-Iteration:" << endl;
+    head = env.init(vec);
+    {
+        IterationSolution is;
+        auto* rhead = is.reverseList(head);
+        env.print(rhead);
+    }
+
+    cout << "reverseList-Recursion:" << endl;
+    head = env.init(vec);
+    {
+        RecursionSolution rs;
+        auto* rhead = rs.reverseList(head);
+        env.print(rhead);
     }
 
     return 0;
--- sample/test/test4.cpp
+++ sample/test/test4.cpp
@@ -1,12 +1,28 @@
-#define MAX ((a > b) ? (a) : (b))
+#include <iostream>
+#include <unordered_set>
+#include <algorithm>
 
-typedef struct A {
-    int a;
-    int b;
-} A;
+struct StringHasher {
+public:
+    size_t operator()(const std::string& s) const {
+        return std::hash<int>()(s.length());
+    }
+};
+
+struct StringComparator {
+public:
+    bool operator()(const std::string& s1, const std::string& s2) const {
+        return s1.length() == s2.length();
+    }
+};
 
 int main() {
-    A a;
+    std::unordered_set<std::string, StringHasher, StringComparator> US;
+    US.insert("a");
+    US.insert("b");
+    US.insert("ccc");
+    US.insert("dddd");
+    for (const std::string& s : US)
+        std::cout << s;
     return 0;
 }
-
